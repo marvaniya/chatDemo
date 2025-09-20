9e4687e3ec7ae1f32f7d3491372dfa98d3769837
Dharmik Marvaniya <dharmikmarvaniya123@gmail.com>
2025-09-20 17:46:20 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route} from 'react-router-dom';
+import '../public/poppins/fonts.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+import { StrictMode } from 'react';
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+)
